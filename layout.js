4cbd1a448014f1312acafad26a7d819a89069549
mistermappy123 <mikhal.gelo@gmail.com>
2017-10-03 16:51:04 -0400

    bare minimum

---
+++ layout.js
@@ -0,0 +1,18 @@
+/*function inputString(string){
+  console.log(string);
+}
+
+function noArguments() {
+  console.log('something');
+}
+*/
+
+function openDoors(doorNum) {
+  if (doorNum == 1) {
+    alert('You\'ve received 500 Gold');
+  }
+  else if (doorNum == 2) {
+    alert('You\'ve received 500 Silver');
+  else (doorNum == 3) {
+      alert('You\'ve received 500 Bronze');
+}
